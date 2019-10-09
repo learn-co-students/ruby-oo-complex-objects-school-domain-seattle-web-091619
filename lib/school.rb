1ca179f9a4075cea2c07c20eb0ee01beca6af945
github username <github email address>
2019-10-09 14:37:05 -0700

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,31 +1,25 @@
 require 'pry'
 
-
 class School
 
-    attr_accessor :name, :roster, :grade
-    def initialize(name)
-        @name = name
+    attr_accessor :roster, :student, :grade
+    def initialize(roster)
         @roster = {}
     end
 
     def add_student(student, grade)
-        if roster[grade] == nil
+        if @roster[grade]== nil
             @roster[grade] = []
         end
         @roster[grade] << student.to_s
-        @roster[grade]
-            #binding.pry
+        @roster
     end
-
+    
     def grade(grade)
         @roster[grade]
     end
 
     def sort
-       roster.each { |grade, students| students.sort! }
+        roster.each { |grade, students| students.sort! }
     end
-end
-
-
-
+end    
